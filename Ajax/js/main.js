49e1b4b9b8df4dba05d92074d7e60572577992c8
Isabelle <isabelle.tieu@gmail.com>
2021-11-10 09:16:38 +0100

    ajax - removed console log

--- Ajax/js/main.js
+++ Ajax/js/main.js
@@ -51,13 +51,11 @@ const target = document.querySelector("#target")
      .catch(error => alert("Erreur : " + error));
  }
 
- 
  /*************************************
  ****   FUNCTION AJAX CALL JSON   *****
  **************************************/
 
  function ajaxCallJSON (url, functionCallback) {
-
      fetch(url)
      .then(response => response.JSON())
      .then(datas => functionCallback(datas))
@@ -79,7 +77,6 @@ const target = document.querySelector("#target")
 function ajaxGetJSON(JSONlist) {
     let html = "<ul>";
     let list = JSON.parse(JSONlist)
-
     list.forEach(item => {
         html += `<li><b>First name :</b> ${item.firstName}</li><br>
         <li><i>Téléphone : </i>${item.phone}</li><br>`
@@ -95,11 +92,8 @@ function ajaxGetJSON(JSONlist) {
 
 
 function ajaxGetJSON2(JSONlist) {
-    console.log(JSONlist);
-
     let html = `<ul class="movie-list">`;
     let list = JSON.parse(JSONlist)
-
     list.forEach(item => {
         html += `<li><img src="images/${item.cover}">
         <p><b>${item.title}</b> - <i>${item.duration}</i></p></li>`
